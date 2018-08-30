ac4d5b3195e3385813cf4ff3b18f98d2c62f94a0
Roslyn Saplicki <roslynsaplicki@Roslyns-MacBook-Pro.local>
2018-08-30 16:01:13 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,33 @@
-alert('Hello, friends.');
 var cards = ["queen", "queen", "king", "king"];
+
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[3];
-cardsInPlay.push('cardOne');
-cardsInPlay.push('cardTwo');
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+	var cardOne = cards[0];
+	cardsInPlay.push('cardOne');
+	console.log("User flipped " + cardOne);
+	var cardTwo = cards[3];
+	cardsInPlay.push('cardTwo');
+	console.log("User flipped " + cardTwo);
+
 var checkForMatch = function () {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
   } else {
     alert("Sorry, try again.");
   }
-} 
+  } ;
+
 if (cardsInPlay.length === 2) {
 	checkForMatch()
-}
+  };
+
+var flipCard = function(cardId) {
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInplay.length === 2) 
+		checkForMatch ()
+	}
+	console.log("User flipped " + cards[cardId]);	
+
+  flipCard(0);
+  flipCard(2);
+
+//cardId isn't defined?
