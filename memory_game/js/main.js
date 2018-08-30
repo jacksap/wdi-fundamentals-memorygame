5c365addf5c80ad9d84da2abf2bb4643d45e4f9d
Roslyn Saplicki <roslynsaplicki@Roslyns-MacBook-Pro.local>
2018-08-30 14:32:00 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,19 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+alert('Hello, friends.');
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[3];
+cardsInPlay.push('cardOne');
+cardsInPlay.push('cardTwo');
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+var checkForMatch = function () {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+} 
+if (cardsInPlay.length === 2) {
+	checkForMatch()
+}
