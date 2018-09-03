4d892abf794d8a3018dc034c3b3c082d5e53a0bc
Roslyn Saplicki <roslynsaplicki@Roslyns-MacBook-Pro.local>
2018-09-03 11:12:58 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,33 +1,47 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank: "Queen",
+        suit: "Hearts",
+        cardImage: "images/queen-of-hearts.png",
+    },
+    {
+        rank: "Queen",
+        suit: "Diamonds",
+        cardImage: "images/queen-of-diamonds.png",
+    },
+    {
+        rank: "King",
+        suit: "Hearts",
+        cardImage: "images/king-of-hearts.png",
+    },
+    {
+        rank: "King",
+        suit: "Diamonds",
+        cardImage: "images/king-of-diamonds.png",
+    }
+];
 
 var cardsInPlay = [];
-	var cardOne = cards[0];
-	cardsInPlay.push('cardOne');
-	console.log("User flipped " + cardOne);
-	var cardTwo = cards[3];
-	cardsInPlay.push('cardTwo');
-	console.log("User flipped " + cardTwo);
 
-var checkForMatch = function () {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.");
-  }
-  } ;
-
-if (cardsInPlay.length === 2) {
-	checkForMatch()
-  };
+var checkForMatch = function() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match");
+    } else {
+        alert("Sorry, try again.");
+    }
+};
 
 var flipCard = function(cardId) {
-	cardsInPlay.push(cards[cardId]);
-	if (cardsInplay.length === 2) 
-		checkForMatch ()
-	}
-	console.log("User flipped " + cards[cardId]);	
+    cardsInPlay.push(cards[cardId].rank);
+    if (cardsInPlay.length === 2) {
+        checkForMatch();
+    }
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+};
 
-  flipCard(0);
-  flipCard(2);
+flipCard(0);
+flipCard(1);
 
 //cardId isn't defined?
